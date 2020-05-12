c7d304e433f2e84373aa880f56fce8a70854386b
Dan Ran <randanfx@gmail.com>
2020-05-12 14:05:26 -0400

    update

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,10 +1,43 @@
   // const webpack = require('webpack')
 
   const routerBase = process.env.DEPLOY_ENV === 'GH_PAGES' ? {
+    head: {
+      title: process.env.npm_package_name || '',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      ],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/szapp/favicon.ico' },
+
+        {rel: 'stylesheet', href:'https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300;400;700&family=Noto+Sans+TC:wght@300;500;700&family=Noto+Serif+JP:wght@300;500&family=Noto+Serif+TC:wght@200;500;600&display=swap" rel="stylesheet'}
+
+
+      ]
+    },
+
     router: {
       base: '/szapp/'
     }
-  } : {}
+  } : {
+    head: {
+      title: process.env.npm_package_name || '',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      ],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+
+        {rel: 'stylesheet', href:'https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300;400;700&family=Noto+Sans+TC:wght@300;500;700&family=Noto+Serif+JP:wght@300;500&family=Noto+Serif+TC:wght@200;500;600&display=swap" rel="stylesheet'}
+
+
+      ]
+    }
+
+  }
 
 
 
@@ -15,21 +48,7 @@ export default {
   /*
   ** Headers of the page
   */
-  head: {
-    title: process.env.npm_package_name || '',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
 
-      {rel: 'stylesheet', href:'https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300;400;700&family=Noto+Sans+TC:wght@300;500;700&family=Noto+Serif+JP:wght@300;500&family=Noto+Serif+TC:wght@200;500;600&display=swap" rel="stylesheet'}
-
-
-    ]
-  },
   /*
   ** Customize the progress-bar color
   */
